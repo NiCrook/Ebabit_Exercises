92651e0d1f1eb4b3a1dccf3959fa2400d1cb1802
NiCrook <nicrook@gmail.com>
2020-11-29 15:04:52 -0600

    first

---
+++ First_Ten/01.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/rZToTkR5eB9Zn4zLh
+#   Create a function that takes two numbers as arguments and return their sum.
+
+
+def addition(int1: int, int2: int) -> int:
+    result = (int1 + int2)
+    return result
+
+
+print(addition(3, 4))
