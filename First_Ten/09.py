298b3131d63e604eb54b5c75153ca27f04fdc1af
NiCrook <nicrook@gmail.com>
2020-11-29 16:00:21 -0600

    Ninth

---
+++ First_Ten/09.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/GPmoRCZKkyNtoJMcN
+#   Create a function that takes a string and returns it as an integer.
+
+
+def numberizer(string: str) -> int:
+    number = int(string)
+    return number
+
+
+print(numberizer("7"))
