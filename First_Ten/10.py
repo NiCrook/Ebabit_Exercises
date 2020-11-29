3852cbca4af66ec013864b9acd17898234302b79
NiCrook <nicrook@gmail.com>
2020-11-29 16:02:16 -0600

    Tenth

---
+++ First_Ten/10.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/xbZR26rHMNo32yz35
+#   Create a function that takes the age and return the age in days.
+
+
+def age_days(years: int) -> int:
+    days = years * 365
+    return days
+
+
+print(age_days(65))
