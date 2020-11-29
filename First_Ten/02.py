a888dbc2f901c9d6dfb56e9c2ad61de01b7fd750
NiCrook <nicrook@gmail.com>
2020-11-29 15:18:35 -0600

    Second

---
+++ First_Ten/02.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/KjCS7occ9hfu5snpb
+#   Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+
+def incrementer(int1: int) -> int:
+    int1 += 1
+    return int1
+
+
+print(incrementer(5))
