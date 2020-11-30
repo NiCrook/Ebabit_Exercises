bcc3eb5089a4f49aeb044805614ad29bad3a8e53
NiCrook <nicrook@comcast.net>
2020-11-29 20:39:42 -0600

    Fourteenth

---
+++ 11-19/14.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/sLkTkfLgZYs5wejsg
+#   make a func that finds the square of a given number
+
+
+def squarer(number: int) -> int:
+    squared = number * number
+    return squared
+
+
+print(squarer(5))
