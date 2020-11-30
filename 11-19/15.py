200c0b2dfd64a19168d42b7313101f602cabdd70
NiCrook <nicrook@comcast.net>
2020-11-29 20:42:15 -0600

    Fifteenth

---
+++ 11-19/15.py
@@ -0,0 +1,10 @@
+#   https://edabit.com/challenge/hEQ3rBrKrztQK8qAd
+#   Create a function that takes a list and returns the first element.
+
+
+def first_index_finder(example_list: list) -> None:
+    first_index = example_list[0]
+    return first_index
+
+
+print(first_index_finder([11, 2, 3]))
