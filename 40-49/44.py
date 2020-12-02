755acd645e7051b8f2683b1f2db387759ac2bb79
NiCrook <nicrook@comcast.net>
2020-12-01 21:29:39 -0600

    Forty Four

---
+++ 40-49/44.py
@@ -0,0 +1,15 @@
+#   https://edabit.com/challenge/aj7JPnAuW8dy4ggdp
+#   Write a function that returns the string "even" if the given integer is even, and the string "odd" if it's odd.
+
+
+def even_or_odd(num1: int) -> str:
+    try:
+        if num1 % 2 == 0:
+            return "Even"
+        else:
+            return "Odd"
+    except TypeError as err:
+        print(f"Error: {err}")
+
+
+print(even_or_odd(5))
